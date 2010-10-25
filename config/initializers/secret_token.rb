5c78cc9309b49d2278d156405661c3e0d815aaa3
wa <wa@Paradigm-LUXs.(none)>
2010-10-24 18:13:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatrickYauHorrorGame::Application.config.secret_token = '225983b9a15f3a3873f22c9ce784aafd1f301038a638a4a652745257af5734610f034c01801b32216bb0ef8969c533a4e879e95ff40cc498e95bd2cfddf2ba82'
